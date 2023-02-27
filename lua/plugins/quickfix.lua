b83df3d192d3d5630f166aceb6db7cd30ed1270f
pierre <dommerc.pierre@gmail.com>
2023-02-27 01:16:20 +0100

    fix: typo

--- lua/plugins/quickfix.lua
+++ lua/plugins/quickfix.lua
@@ -2,7 +2,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
--- ⚡ enhanced quickfix/loclist
+-- ⚡ enhanced qflist/loclist
 
 local group_id = vim.api.nvim_create_augroup('CustomQuickfix', {})
 
