7c18a609bc82d7dc8e5554530e3e3b5fb53ccd8e
pierre <dommerc.pierre@gmail.com>
2025-02-22 11:30:19 +0100

    fix multiple encoding warnings

--- lua/lsp/common.lua
+++ lua/lsp/common.lua
@@ -6,6 +6,15 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 require('cmp_nvim_lsp').default_capabilities(capabilities)
 
+-- fix a warning "multiple different client offset_encodings detected"
+-- see https://github.com/neovim/nvim-lspconfig/issues/2184
+capabilities = vim.tbl_deep_extend('force', capabilities, {
+  offsetEncoding = { 'utf-16' },
+  general = {
+    positionEncodings = { 'utf-16' },
+  },
+})
+
 local M = {
   capabilities = capabilities,
 }
